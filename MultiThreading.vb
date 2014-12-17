8d9c00aca7f4f31bf98123799a388b71bfc5f18e
marcus lindh <marcusl1@hotmail.com>
2014-12-17 15:55:35 +0100

    added functions to class

---
+++ MultiThreading.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Threading
+
+Public Class MultiThreading
+
+    Private context As SynchronizationContext
+
+
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub Run(ByVal fun As Action)
+
+        context = SynchronizationContext.Current
+
+
+        Task.Factory.StartNew(fun)
+
+    End Sub
+
+    Public Sub Run(ByVal fun As Action, after As Action)
+
+        context = SynchronizationContext.Current
+
+
+        Task.Factory.StartNew(fun).ContinueWith(Sub() after())
+
+    End Sub
+
+    Public Sub ThreadSafe(ByVal data As Object, ByVal func As SendOrPostCallback)
+        context.Post(func, data)
+    End Sub
+
+End Class
