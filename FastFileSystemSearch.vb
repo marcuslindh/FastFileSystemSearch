5236800d042630adff98214cce2fb729dbbf00a5
marcus lindh <marcusl1@hotmail.com>
2015-01-17 21:11:19 +0100

    update

--- FastFileSystemSearch.vb
+++ FastFileSystemSearch.vb
@@ -1,59 +1,15 @@
 ﻿Public Class Search
 
-    Public Property Settings As New Settings
-
-    Public Sub Search(StartPath As String, Files As Action(Of IO.FileInfo), Folder As Action(Of IO.DirectoryInfo))
-
-        Dim multi As New MultiThreading
-
-        multi.Run(Sub()
-                      Enumerate(StartPath, Files, Folder, multi)
-                  End Sub)
-
-
+       Public Sub EnumerateDirectories(RootDirectory As IO.DirectoryInfo, Directory As Action(Of IO.DirectoryInfo))
+        For Each item In RootDirectory.EnumerateDirectories
+            Directory(item)
+        Next
     End Sub
-    'Public Sub Search(StartPath As String, Files As Action(Of IO.FileInfo), Folder As Action(Of IO.DirectoryInfo))
-    'End Sub
-
-    Private Sub Enumerate(path As String, Files As Action(Of IO.FileInfo), Folder As Action(Of IO.DirectoryInfo), multi As MultiThreading)
-        Dim _folder As New IO.DirectoryInfo(path)
-
-        If Settings.SearchSubFolders Then
-            If Settings.RunEventForFolders Then
-                For Each item In _folder.EnumerateDirectories
-                    multi.ThreadSafe(item, Sub(dir As IO.DirectoryInfo)
-                                               Folder(dir)
-                                           End Sub)
-                    Enumerate(item.FullName, Files, Folder, multi)
-                Next
-            Else
-                For Each item In _folder.EnumerateDirectories
-                    Enumerate(item.FullName, Files, Folder, multi)
-                Next
-            End If
-
-        End If
-
-        If Settings.RunEventForFiles Then
-            For Each item In _folder.EnumerateFiles
-                multi.ThreadSafe(item, Sub(fil As IO.FileInfo)
-                                           Files(fil)
-                                       End Sub)
-            Next
-        End If
-
+    Public Sub EnumerateFiles(RootDirectory As IO.DirectoryInfo, File As Action(Of IO.FileInfo))
+        For Each item In RootDirectory.EnumerateFiles
+            File(item)
+        Next
     End Sub
-
-
-
-
-End Class
-
-Public Class Settings
-
-    Public Property SearchSubFolders As Boolean = True
-    Public Property RunEventForFolders As Boolean = True
-    Public Property RunEventForFiles As Boolean = True
 
 
 End Class
